51ed24beabf38a17e14bc1aed570998fa18cc442
diegogauto <diegogauto@yahoo.com.ar>
2022-11-18 22:07:59 -0300

    Load errors fixed

--- js/login.js
+++ js/login.js
@@ -1,59 +1,48 @@
-//import userExist,{contraseñaCorrecta} from "./utils"; 
+import userExist, { contraseñaCorrecta } from "./utils.js";
 
-const usuarios = [{email:'maradona@gmail', pass: '123' }, {email:'rodolfo@gmail', pass: '123'} ] 
+const usuarios = [
+  { email: "maradona@gmail", pass: "123" },
+  { email: "rodolfo@gmail", pass: "123" },
+];
 
-localStorage.setItem('usuarios',JSON.stringify(usuarios))
+localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-function userExist(userEmail, users) {
-return users.some((user) => user.email == userEmail)
+/*function userExist(userEmail, users) {
+  return users.some((user) => user.email == userEmail);
 }
 
- const contraseñaCorrecta =(contraseña,email,users)=> {
- return contraseña==getPass (email,users)
-  
-}
+const contraseñaCorrecta = (contraseña, email, users) => {
+  return contraseña == getPass(email, users);
+};
 
 const getPass = (userEmail, users) => {
-  return users.find((user) => user.email == userEmail).pass
-}
+  return users.find((user) => user.email == userEmail).pass;
+};*/
 
+const formLogin = document.getElementById(`formulario`);
 
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById(`inputEmail`).value;
+  const pass = document.getElementById(`inputPassword`).value;
 
-const buttonEnviar = document.getElementById (`enviar`);
-
-buttonEnviar.addEventListener ('click', (e)=>{
-
-e.preventDefault();
-const email= document.getElementById (`inputEmail`).value;
-const pass= document.getElementById (`inputPassword`).value;
-
-
-const usuarios = JSON.parse (localStorage.getItem ('usuarios'));
-console.log (usuarios);
-if (! userExist(email,usuarios)){ alert ("usuario no existente")
-
-} else if (!contraseñaCorrecta (pass,email,usuarios)){ alert ("contraseña incorrecta")} 
-
-else { 
+  const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  console.log(usuarios);
+  if (!userExist(email, usuarios)) {
+    alert("Credenciales incorrectas");
+  } else if (!contraseñaCorrecta(pass, email, usuarios)) {
+    alert("Credenciales incorrectas");
+  } else {
     localStorage.setItem(
-        'currentUser',
-        JSON.stringify ({
-          email: email,
-          pass: pass
-        }))
-    
-    
-        formLogin.reset();
-        window.location.href = '../index.html';
-    
+      "currentUser",
+      JSON.stringify({
+        email: email,
+        pass: pass,
+      })
+    );
 
-
-
-}
-
-
-
-
+    formLogin.reset();
+    alert("Login Exitoso");
+    window.location.href = "../index.html";
+  }
 });
-
-
--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,10 @@
 export default function userExist(userEmail, users) {
-    return users.some((user) => user.email === userEmail)
-  }
-
-export const contraseñaCorrecta =(contraseña,email,users)=> {
-   return contraseña===getPass (email,users)
-    
+  return users.some((user) => user.email === userEmail);
 }
 
-const getPass = (userEmail, users) => {
-    return users.find((user) => user.email === userEmail).pass
-  }
+export const contraseñaCorrecta = (contraseña, email, users) => {
+  const getPass = (userEmail, users) => {
+    return users.find((user) => user.email === userEmail).pass;
+  };
+  return contraseña === getPass(email, users);
+};
